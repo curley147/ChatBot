23e0504204765bcaf813a2aba60cb41b7bc74b5c
Micheal Curley <G00340243@gmit.ie>
2017-11-24 10:01:42 +0000

    reflection added

--- Chatbot.go
+++ Chatbot.go
@@ -5,10 +5,10 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"math/rand"
 	"net/http"
 	"regexp"
+	"strings"
 	"time"
 )
 
@@ -21,19 +21,21 @@ func ChatResponse(input string) string {
 		"Why do you say that?",
 	}
 	//regexp strings to look for certain patterns
-	reObjs := [6]string{
-		//check for father regardless of lower/upper case
-		"(?i).*\b[Ff]ather\b.*",
+	reObjs := [7]string{
+		//check for how are you? from the user
+		"(?i).*how are you\\?*.*",
 		//check for "I am", "I'm", "Iam", "i'm", "i am", "iam" in input string and capturing data
-		"(?i)(I[' a]*m)([^.?!]*)[.?!]?",
+		"(?i)(.*I[' a]*m)([^.?!]*)[.?!]?",
 		//check for "i think" and capturing data
-		"(?i)(I think)([^.?!]*)[.?!]?",
+		"(?i)(.*I think)([^.?!]*)[.?!]?",
 		//check for you and ?
 		"(?i)(you+)(.*)(\\?+)",
 		//check for "i like" and capturing data
-		"(?i)(I like)(\\s+)[.?!]?",
+		"(?i)(.*I like)(\\s+)[.?!]?",
 		//check for numbers
 		"\\d",
+		//check for how are you? from the user
+		"(?i).*hello\b.*",
 	}
 	//for loop iterating over regexp strings and compiling them
 	for i := 0; i < len(reObjs); i++ {
@@ -44,8 +46,8 @@ func ChatResponse(input string) string {
 		if match == true {
 			switch i {
 			case 0:
-				//father response
-				return "Why don’t you tell me more about your father?"
+				//greeting response
+				return "I'm very well thank you for asking, how are you?"
 			case 1:
 				//replacing start of response and adding captured data
 				return re.ReplaceAllString(input, "How do you know you are $2?")
@@ -60,38 +62,58 @@ func ChatResponse(input string) string {
 			case 5:
 				//numbers response
 				return "I'm not a fan of numbers, can you simplify it in any way?"
+			case 6:
+				//hello response
+				return "Hello there again!"
 			default:
 				return "Error message"
 			}
 		}
 	}
-	return responses[random(0, len(responses))]
-}
-
-//random number generator method
-func random(min, max int) int {
 	rand.Seed(time.Now().Unix())
-	return rand.Intn(max-min) + min
+	randNum := rand.Intn(len(responses))
+	return responses[randNum]
 }
 
-//Chatdata struct
-type Chatdata struct {
-	UserInput string
-	Response  string
-}
-
-func chatBotHandler(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("template/chatBot.html")
-	if err != nil {
-		fmt.Println("template retrieval failed:", err)
+//Reflect Adapted from https://gist.github.com/ianmcloughlin/c4c2b8dc586d06943f54b75d9e2250fe
+//Pronouns reflection method
+func Reflect(input string) string {
+	// Split the input on word boundaries.
+	boundaries := regexp.MustCompile(`\b`)
+	tokens := boundaries.Split(input, -1)
+	// List the reflections.
+	reflections := [][]string{
+		{"your", "my"},
+		{"you", "I"},
+		{"me", "you"},
 	}
-	t.Execute(w, Chatdata{UserInput: ""})
+	// Loop through each token, reflecting it if there's a match.
+	for i, token := range tokens {
+		for _, reflection := range reflections {
+			if matched, _ := regexp.MatchString(reflection[0], token); matched {
+				tokens[i] = reflection[1]
+				break
+			}
+		}
+	}
+	// Put the tokens back together.
+	return strings.Join(tokens, ``)
+}
+func chatBotHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.Header.Get("value"))
+	input := r.Header.Get("value")
+	fmt.Fprintf(w, "You: %s\n", input)
+	response := Reflect(input)
+	refResponse := ChatResponse(response)
+	fmt.Fprintf(w, "ChatterBot: %s\n", refResponse)
 }
 
 //main
 func main() {
+	//static file handler
+	http.Handle("/", http.FileServer(http.Dir("./html")))
 	//template file handler
-	http.HandleFunc("/", chatBotHandler)
+	http.HandleFunc("/postSend", chatBotHandler)
 	//Listen for requests on port 80
 	http.ListenAndServe(":8080", nil)
 }
